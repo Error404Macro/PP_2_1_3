2048c09d3cba2aef6270fcde3f8cef57bdd97817
Error <ya.gaukler@gmail.com>
2023-12-12 23:13:51 +0300

    The dog has been added

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,22 @@
 package app.config;
 
+import app.model.Animal;
+import app.model.Dog;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+    @Bean
+    public Timer timer() {
+        return new Timer();
+    }
+    @Bean
+    public Animal dog() {
+        return new Dog();
+    }
+
 }
